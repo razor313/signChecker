8cede9e5ab2c1038648763774eea1123a25d0ccb
Reza Dehnavi <razornfs@gmail.com>
2020-10-31 09:00:27 +0330

    Initial Commit

---
+++ signChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signChecker
+//
+//  Created by Reza Dehnavi on 10/31/20.
+//  Copyright © 2020 Reza Dehnavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
